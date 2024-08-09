80a3f94b337654da4845583eb9c597897bbe07fc
sr_wandersson <161793765+wanderssong@users.noreply.github.com>
2024-08-09 15:43:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 function toggleMenu() {
-    alert('Menu clicked! Aqui colocarei a logica.');
+    document.body.classList.toggle('menu-open');
 }
 
-function redirectToWhatsApp() {
+// Função principal para redirecionar para o WhatsApp com uma mensagem específica
+function redirectToWhatsApp(option) {
+    let message = "";
+
+    switch(option) {
+        case 'orçamento':
+            message = "OLÁ, GOSTARIA DE SOLICITAR UM ORÇAMENTO!";
+            break;
+        case 'conhecer':
+            message = "OLÁ, GOSTARIA DE CONHECER MAIS SOBRE O SISTEMA!";
+            break;
+        case 'visita':
+            message = "OLÁ, GOSTARIA DE SOLICITAR UMA VISITA DE UM REPRESENTANTE!";
+            break;
+    }
+
+    const phoneNumber = "6699247321"; // Insira o número do WhatsApp com o código do país
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.location.href = url;
+}
+
+// Função específica para o botão "Contrate já!"
+function redirectToWhatsAppTEF() {
     const phoneNumber = '6699247321'; // Substitua pelo número da sua empresa no formato internacional
     const message = 'Olá, gostaria de saber mais sobre o TEF!';
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
@@ -10,6 +32,7 @@ function redirectToWhatsApp() {
     window.location.href = whatsappUrl;
 }
 
+// Função específica para o ícone do WhatsApp
 function redirectToWhatsAppSystem() {
     const phoneNumber = '6699247321'; // Substitua pelo número da sua empresa no formato internacional
     const message = 'OLÁ, GOSTARIA DE SABER MAIS SOBRE O SISTEMA ERGONTECH';
